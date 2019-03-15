b44228a5290afc7a7ba10510834c9f2cfa612775
faide <faider29@mail.ru>
2019-03-15 13:22:23 +0300

    15/03/2019 added layout for newspaper

--- app/src/main/java/com/andrienkom/vertolapp/mvvm/fragments/FragmentNewspaper.java
+++ app/src/main/java/com/andrienkom/vertolapp/mvvm/fragments/FragmentNewspaper.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,10 @@ import com.andrienkom.vertolapp.R;
 public class FragmentNewspaper extends Fragment {
 
     private View mToolbar;
-    private TextView mText;
+    private RecyclerView mRVHorizontal;
+    private RecyclerView mRVVertical;
+
+
 
     public static FragmentNewspaper newInstance(){
         return new FragmentNewspaper();
@@ -27,7 +31,8 @@ public class FragmentNewspaper extends Fragment {
         View view = inflater.inflate(R.layout.fr_newspaper,container,false);
 
         mToolbar = view.findViewById(R.id.fr_newspaper_custom_toolbar);
-        mText = view.findViewById(R.id.tv_newspaper_working);
+        mRVHorizontal = view.findViewById(R.id.rv_newspaper_issue);
+        mRVVertical = view.findViewById(R.id.rv_newspaper_news);
 
         return view;
     }
